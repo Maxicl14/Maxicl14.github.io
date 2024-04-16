9bdc7852947ed6a7d2f34c3d8f302da1d21457e3
Maxicl14 <50806684+Maxicl14@users.noreply.github.com>
2024-04-16 13:32:53 +0100

    Create Objects.js

---
+++ A-spherical-world-project/modules/Objects.js
@@ -0,0 +1,10 @@
+class Object4D {
+  constructor(pvuw, size, model_3D){
+    this.ID = ID;
+    this.pvuw = pvuw;
+    this.size = size;
+    this.model_3D = model_3D;
+  }
+}
+
+export { Object4D };
